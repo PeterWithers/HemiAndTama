c9721c1d2934a39c1ca5fb27a89035a8ac41b88b
Peter Withers <peter-gthb@bambooradical.com>
2014-10-31 22:40:12 +0100

    Wheel now turns on contact with player. #15

--- HemiAndTama/Assets/HamsterWheel.cs
+++ HemiAndTama/Assets/HamsterWheel.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class HamsterWheel : MonoBehaviour {
 	float maxSpeed = 100f;
 	float momentum = 50f;
-	float decay = 0.1f;
+	float decay = 0.5f;
 
 	public void setSpeed(float speed) {
 		// speed is a value from -1 to 1 where 0 is stationary
+		print ("speed: " + speed);
 		momentum = maxSpeed * speed;
 	}
 
 	void Update() {
 		momentum = (momentum > 0)? momentum - decay : 0;
-		Debug.Log ("momentum: " + momentum);
+		//Debug.Log ("momentum: " + momentum);
 		transform.Rotate(Vector3.back, Time.deltaTime * momentum);
 	}
 
