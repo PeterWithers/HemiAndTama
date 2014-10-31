f14b9ddd82f8614297fa124d4344b6eb0387e52f
Peter Withers <peter-gthb@bambooradical.com>
2014-10-31 20:55:45 +0100

    Started to add a hamster wheel rotation script based on player interaction. #15 Updated the git ignore list. Modified the hamster wheel and replaced the prefab. #14

---
+++ HemiAndTama/Assets/HamsterWheel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HamsterWheel : MonoBehaviour {
+	float maxSpeed = 100f;
+	float momentum = 50f;
+	float decay = 0.1f;
+
+	public void setSpeed(float speed) {
+		// speed is a value from -1 to 1 where 0 is stationary
+		momentum = maxSpeed * speed;
+	}
+
+	void Update() {
+		momentum = (momentum > 0)? momentum - decay : 0;
+		Debug.Log ("momentum: " + momentum);
+		transform.Rotate(Vector3.back, Time.deltaTime * momentum);
+	}
+
+	void FixedUpdate() {
+	}
+}
